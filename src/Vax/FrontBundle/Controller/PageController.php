46b3516521402a0509e2b936e48456c0a986f563
juampa <jpasosa@gmail.com>
2012-09-17 19:59:14 -0300

    secciones definidas.subsecciones también

--- src/Vax/FrontBundle/Controller/PageController.php
+++ src/Vax/FrontBundle/Controller/PageController.php
@@ -25,30 +25,68 @@ class PageController extends Controller
         return $this->render('VaxFrontBundle:Page:history.html.twig');
     }
 
+
+
+
+
     // SERVICIOS
     public function serviceAction() {
         return $this->render('VaxFrontBundle:Page:service.html.twig');
     }
+
+    // SERVICIOS -- MOTHERBOARD
+    public function motherAction() {
+        return $this->render('VaxFrontBundle:Page:mother.html.twig');
+    }
+
+    // SERVICIOS -- NOTEBOOK
+    public function notebookAction() {
+        return $this->render('VaxFrontBundle:Page:notebook.html.twig');
+    }
+
+    // SERVICIOS -- PLACAS DE VIDEO
+    public function videocardAction() {
+        return $this->render('VaxFrontBundle:Page:videocard.html.twig');
+    }
+
+    // SERVICIOS -- SEGURIDAD Y COMUNICACIONES
+    public function securityAction() {
+        return $this->render('VaxFrontBundle:Page:security.html.twig');
+    }
+
+
+
 
     // NUESTROS CLIENTES
     public function clientsAction() {
         return $this->render('VaxFrontBundle:Page:clients.html.twig');
     }
     
+
+
     // UBIQUITI
     public function ubiquitiAction() {
         return $this->render('VaxFrontBundle:Page:ubiquiti.html.twig');
     }
+
+
+
 
     // OFERTAS
     public function dealsAction() {
         return $this->render('VaxFrontBundle:Page:deals.html.twig');
     }
 
+
+
+
     // FAQ
     public function faqAction() {
         return $this->render('VaxFrontBundle:Page:faq.html.twig');
     }
+
+
+
 
     // CONTACTO
     public function contactAction() {
