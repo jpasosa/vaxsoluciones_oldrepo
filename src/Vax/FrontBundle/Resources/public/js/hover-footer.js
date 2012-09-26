cf2c116503f92e7b5c0907b8e7a01f1eb983bb03
juampa <jpasosa@gmail.com>
2012-09-26 12:19:52 -0300

    imagenes y maquetado slide-vertical d/ofertas

---
+++ src/Vax/FrontBundle/Resources/public/js/hover-footer.js
@@ -0,0 +1,130 @@
+$(window).load(function() {
+    $('.logo1').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo1.hover.png');
+    });
+    $('.logo1').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo1.png');
+    });
+
+    $('.logo2').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo2.hover.png');
+    });
+    $('.logo2').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo2.png');
+    });
+
+    $('.logo3').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo3.hover.png');
+    });
+    $('.logo3').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo3.png');
+    });
+
+    $('.logo4').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo4.hover.png');
+    });
+    $('.logo4').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo4.png');
+    });
+
+    $('.logo5').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo5.hover.png');
+    });
+    $('.logo5').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo5.png');
+    });
+
+    $('.logo6').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo6.hover.png');
+    });
+    $('.logo6').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo6.png');
+    });
+
+    $('.logo7').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo7.hover.png');
+    });
+    $('.logo7').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo7.png');
+    });
+
+    $('.logo8').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo8.hover.png');
+    });
+    $('.logo8').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo8.png');
+    });
+
+    $('.logo9').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo9.hover.png');
+    });
+    $('.logo9').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo9.png');
+    });
+
+    $('.logo10').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo10.hover.png');
+    });
+    $('.logo10').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo10.png');
+    });
+
+    $('.logo11').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo11.hover.png');
+    });
+    $('.logo11').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo11.png');
+    });
+
+    $('.logo12').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo12.hover.png');
+    });
+    $('.logo12').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo12.png');
+    });
+
+    $('.logo13').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo13.hover.png');
+    });
+    $('.logo13').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo13.png');
+    });
+
+    $('.logo14').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo14.hover.png');
+    });
+    $('.logo14').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo14.png');
+    });
+
+    $('.logo15').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo15.hover.png');
+    });
+    $('.logo15').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo15.png');
+    });
+
+    $('.logo16').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo16.hover.png');
+    });
+    $('.logo16').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo16.png');
+    });
+
+    $('.logo17').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo17.hover.png');
+    });
+    $('.logo17').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo17.png');
+    });
+
+    $('.logo18').mouseover(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo18.hover.png');
+    });
+    $('.logo18').mouseout(function() {
+      $(this).attr('src','/bundles/vaxfront/images/footer/logo18.png');
+    });
+
+
+
+});
